b60fd74222fa4f84c3db43c405954014674e65a8
DevMarkLights <90109613+DevMarkLights@users.noreply.github.com>
2021-10-02 22:38:19 -0400

    Database is fixed and finalized

--- app/src/main/java/com/example/myapplication/Database.java
+++ app/src/main/java/com/example/myapplication/Database.java
@@ -10,8 +10,6 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Locale;
-
 public class Database extends AppCompatActivity {
 
     Button button2;
@@ -19,7 +17,8 @@ public class Database extends AppCompatActivity {
     Button addStock, deleteStock, updatestock, viewStocksList;
     DBHelper DB;
     String date_of_dividend;
-    double annualDividend,Dividend_Yield, price,amount_of_dividend, average_cost;
+    double annualDividend,Dividend_Yield, price,amount_of_dividend, average_cost,percentage;
+    static String fre, freq;
 
     ApiCalls api = new ApiCalls();
 
@@ -31,11 +30,12 @@ public class Database extends AppCompatActivity {
         shares = findViewById(R.id.shares);
         costBasis = findViewById(R.id.costBasis);
         frequency = findViewById(R.id.frequency);
-
         addStock = findViewById(R.id.addStock);
         deleteStock = findViewById(R.id.deleteStock);
         updatestock = findViewById(R.id.updateStock);
         viewStocksList = findViewById(R.id.viewStockList);
+
+
 
         DB = new DBHelper(this);
 
@@ -51,39 +51,38 @@ public class Database extends AppCompatActivity {
                 api.price(symbolTXT);
                 amount_of_dividend = ApiCalls.amount_of_dividend;
                 price = ApiCalls.stock_price;
-
                 date_of_dividend = ApiCalls.date_of_dividend;
-
                 String s = shares.getText().toString();
                 double sharesTXT = Double.parseDouble(s);
-
                 String fre = frequency.getText().toString();
-                fre.toUpperCase(Locale.ROOT);
-                     if(fre.startsWith("M")) {
-                    annualDividend = amount_of_dividend * 12;
-                    }
-                    else if (fre.startsWith("Q")) {
-                    annualDividend = amount_of_dividend * 4;
-                     }
-                     else if (fre.startsWith("S")) {
-                    annualDividend = amount_of_dividend * 2;
-                    }
-                     else {
-                    annualDividend = amount_of_dividend;
-                    }
+                api.getAnnualDividend(fre);
+                api.getDividendYield();
+                // getting the frequency based off user input
+                if(fre.startsWith("M")) {
+                    freq = "Monthly";
+                } else if (fre.startsWith("Q")) {
+                    freq = "Quarterly";
+                }
+                else if (fre.startsWith("S")) {
+                    freq = "Semi-annually";
+                }
+                else {
+                    freq = "annually";
+                }
 
-                double DY = annualDividend / price;
-                Dividend_Yield = DY * 100;
                 // converting the given string representation to double value
                 String c = costBasis.getText().toString();
                 double costBasisTXT = Double.parseDouble(c);
                 average_cost = costBasisTXT;
+                api.profit_loss(sharesTXT,costBasisTXT);
+                percentage =api.percentage_profit_loss;
 
-                boolean checkaddstock = DB.addStock(symbolTXT, sharesTXT, average_cost, fre);
+                boolean checkaddstock = DB.addStock(symbolTXT, sharesTXT, average_cost);
                 if (checkaddstock == true) {
                     Toast.makeText(Database.this, "New stock added", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(Database.this, "New stock not added", Toast.LENGTH_SHORT).show();
+                }
+                 else {
+                    Toast.makeText(Database.this, "Stock already in portfolio", Toast.LENGTH_SHORT).show();
                 }
 
             }
@@ -114,6 +113,7 @@ public class Database extends AppCompatActivity {
                 String c = costBasis.getText().toString();
                 double costBasisTXT = Double.parseDouble(c);
 
+
                 boolean updatestock = DB.updateStock(symbolTXT, sharesTXT, costBasisTXT);
                 if (updatestock == true) {
                     Toast.makeText(Database.this, "New stock updated", Toast.LENGTH_SHORT).show();
@@ -135,15 +135,16 @@ public class Database extends AppCompatActivity {
                 StringBuffer buffer = new StringBuffer();
                 while (res.moveToNext()) {
                     buffer.append("symbol: " + res.getString(0) + "\n");
-                    buffer.append("shares:" + res.getString(1) + "\n");
-                    buffer.append("shares:" + res.getString(2) + "\n");
+                    buffer.append("price: " +"$"+ res.getString(1) + "\n");
+                    buffer.append("shares: " + res.getString(2) + "\n");
                     buffer.append("average cost:" + res.getString(3) + "\n");
-                    buffer.append("dividend: " + res.getString(4) + "\n");
-                    buffer.append("annual dividend: " + res.getString(5) + "\n");
-                    buffer.append("date of dividend: " + res.getString(6) + "\n");
-                    buffer.append("dividend yield: " + res.getString(7) + "\n\n");
-
-
+                    buffer.append("Profit/Loss: " + res.getString(4)+"\n");
+                    buffer.append("dividend: " + res.getString(5) + "\n");
+                    buffer.append("Dividend yield: " + res.getString(6)+ "%" + "\n");
+                    buffer.append("annual dividend: " + res.getString(7) + "\n");
+                    buffer.append("Frequency: " + res.getString(8) + "\n");
+                    buffer.append("date of dividend: " + res.getString(9) + "\n");
+                    buffer.append("market Value: " + res.getString(10)+"\n\n");
                 }
                 AlertDialog.Builder builder = new AlertDialog.Builder(Database.this);
                 builder.setCancelable(true);
